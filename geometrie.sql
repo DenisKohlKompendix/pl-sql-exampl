3718187b76f6d9f356e2d638cad1702e877c7910
User400 <user400@edu.bln>
2022-02-04 09:09:21 +0100

    Beispiel für PL/sql packages

---
+++ geometrie.sql
@@ -0,0 +1,42 @@
+create or replace PACKAGE GEOMETRIE
+as
+    type rechteck_daten is RECORD(a number, b number);
+    KLEINER_NULL EXCEPTION;
+    KLEINER_NULL_ERROR_CODE pls_INTEGER := -20932; -- hat nur die aufgabe den fehler code innerhalt des packages bekannt zumachen 
+    PRAGMA EXCEPTION_INIT(KLEINER_NULL, -20932);
+    MY_PI number := 3.14;
+    FUNCTION area(a number, b number)return number;
+    FUNCTION kreis(r number ) return number;
+    -- neu
+    procedure p_area(a number, b number, o out number);
+END;
+/
+
+
+create or replace PACKAGE BODY GEOMETRIE
+as 
+   FUNCTION area(a number, b number)return number
+   as
+   BEGIN
+    if a < 0 or b < 0 then
+        raise KLEINER_NULL;
+    end if;
+    RETURN a * b;
+   END;
+   
+   function kreis(r number) return number
+   as 
+   BEGIN
+    if r < 0 then 
+        raise KLEINER_NULL;
+    END IF;
+    RETURN r * r * my_pi;
+   END;
+   
+   procedure p_area(a in number, b in number, o out number)
+   as
+   begin
+        o := area(a,b);
+   end;
+END;
+/
